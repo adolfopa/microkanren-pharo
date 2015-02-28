567fcc6bec11e0880c7bd2c1924e99f64979aff4
Adolfo Pérez Álvarez <adolfo.pa@gmail.com>
2015-02-28 13:55:12 +0100

    Use env var to decide how to install dependencies
    Depending on the value of `PHARO_DEP_MGMT` resolve and install
    dependencies in two different ways:
    
      - `zeroconf`, delegate everything into the `travis` ZeroConf script.
    
      - `ubuntu`, assume it is running on Ubuntu 12.04, and install a
        hardcoded set of dependencies.

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
-sudo apt-get update
+case ${PHARO_DEP_MGMT:-zeroconf} in
+    zeroconf)
+	curl http://get.pharo.org/travis | bash ;;
+    ubuntu)
+	sudo apt-get update
 
-sudo apt-get install \
-     libasound2:i386 \
-     libc6:i386 \
-     libfreetype6:i386 \
-     libgl1-mesa-glx:i386 \
-     libssl1.0.0:i386 \
-     libx11-6:i386
+	sudo apt-get install \
+	    libasound2:i386 \
+	    libc6:i386 \
+	    libfreetype6:i386 \
+	    libgl1-mesa-glx:i386 \
+	    libssl1.0.0:i386 \
+	    libx11-6:i386
+	;;
+    *)
+	echo "Invalid dependency resolution: ${PHARO_DEP_MGMT}"
+	echo "Set PHARO_DEP_MGMT to one of the following:"
+	echo "  - zeroconf, use Pharo ZeroConf scripts to resolve and install"
+	echo "    all dependencies. This is the default value."
+        echo "  - ubuntu, assume system is Ubuntu 12.04; the set packages to"
+	echo "    install is hardcoded in this script."
+
+	exit 64 # EX_USAGE, see sysexits(3)
+esac
